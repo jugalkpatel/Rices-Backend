23dd82476f53d47b8f584e1bb5ce0a8fbff0d3bc
jugal <ptjimmy99@gmail.com>
2022-06-13 12:31:44 +0530

    fix: add secure attribute manually

--- src/utils/cookie-utils.ts
+++ src/utils/cookie-utils.ts
@@ -9,8 +9,8 @@ export enum CookieNames {
 
 const cookieOptions: CookieOptions = {
   httpOnly: true,
-  secure: process.env.NODE_ENV === "production",
-  sameSite: false,
+  secure: true,
+  sameSite: "none",
 };
 
 export function setCookies(
