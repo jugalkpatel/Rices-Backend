499e8c44bfb3b9333c9557a4d67521e149213663
jugal <ptjimmy99@gmail.com>
2022-06-13 12:23:24 +0530

    refactor: add sameSite attrib

--- src/utils/cookie-utils.ts
+++ src/utils/cookie-utils.ts
@@ -10,6 +10,7 @@ export enum CookieNames {
 const cookieOptions: CookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
+  sameSite: false,
 };
 
 export function setCookies(
